163676c2b8a69efcaa7da90816668825e349d0cf
vladovel <vladovel@gmail.com>
2018-08-16 09:00:28 +0200

    Initial Commit

---
+++ LBTA_YouTube/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LBTA_YouTube
+//
+//  Created by Vlado Velkovski on 8/16/18.
+//  Copyright © 2018 Vlado Velkovski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
